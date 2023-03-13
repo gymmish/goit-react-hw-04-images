ad7365a02f36385349c86392d4287cd9a45e0738
AS <88048844+gymmish@users.noreply.github.com>
2023-03-13 19:25:13 +0200

    .

---
+++ src/components/App.jsx
@@ -0,0 +1,69 @@
+import { useState, useEffect } from 'react';
+import getImages from './Api/Api';
+import { toast, Toaster } from 'react-hot-toast';
+import Loader from './LoadMore/Loader';
+import SearchBar from './Searchbar/Searchbar';
+import Modal from './Modal/Modal';
+import ImageGallery from './ImageGallery/ImageGallery';
+import ButtonMore from './LoadMore/ButtonL';
+
+export default function App() {
+  const [textSearch, setTextsearch] = useState('');
+  const [page, setPage] = useState(1);
+  const [images, setImages] = useState([]);
+  const [status, setStatus] = useState('idle');
+  const [modalImg, setModalImg] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    if (!textSearch) {
+      return;
+    }
+    setStatus('pending');
+    getImages(textSearch, page)
+      .then(data => data.hits)
+      .then(response => {
+        if (response.length === 0) {
+          toast.error("Sorry, we didn't find anything");
+          setStatus({ status: 'idel' });
+        } else {
+          setImages(prevImages => [...prevImages, ...response]);
+          setStatus('resolved');
+        }
+      })
+      .catch(error => console.log(error));
+  }, [page, textSearch]);
+
+  const handleSubmit = textSearch => {
+    setTextsearch(textSearch);
+  };
+
+  const hendleMore = () => {
+    setPage(page + 1);
+  };
+
+  const FindmodalImg = (id, img, tags) => {
+    setModalImg({ id: id, img: img, tags: tags });
+  };
+
+  const toggleModal = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <>
+      <Toaster position="top-center" reverseOrder={false} />
+      <SearchBar onSearch={handleSubmit} />
+      {status === 'pending' && <Loader />}
+      <ImageGallery
+        response={images}
+        onImageClick={FindmodalImg}
+        toggleModal={toggleModal}
+      />
+      {images.length >= 12 && (
+        <ButtonMore loadMoreClick={hendleMore}></ButtonMore>
+      )}
+      {isOpen && <Modal modalImg={modalImg} onClose={toggleModal} />}
+    </>
+  );
+}
